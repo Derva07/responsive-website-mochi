9c308ed3bb884f02351bb01ca5c5c32c44605daa
Derva07 <dervaag1122@gmail.com>
2023-07-16 11:34:42 +0700

    melakukan update lanjutan

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Menghilangkan sidebar ketika di klik dimana saja
+// Menghilangkan popup ketika di klik dimana saja
 const hamburger = document.querySelector("#hamburger-menu");
+const searchBtn = document.querySelector("#search-btn");
+
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
+
+// Toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+// Ketika di klik search btn
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
